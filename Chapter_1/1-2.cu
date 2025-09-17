10a8fc3313eb2d83322db08a588d97d366e9d3c0
Shixingguo <shixg@ShixingguodeMacBook-Air.local>
2025-09-17 10:12:34 +0800

    Add Chapter_1 and CUDA flies

---
+++ Chapter_1/1-2.cu
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+_global_ void helloFromGPU(void){
+    printf("Hello World from GPU!\n");
+}
+int main(){
+    printf("Hello World from CPU!\n");
+    helloFromGPU <<<1,10>>>();
+    cudaDeviceReset();
+    return 0;
+
+}
