32fd6039a51221ba231216a170b7ff822901a6b1
Shixingguo <shixg@ShixingguodeMacBook-Air.local>
2025-09-17 10:20:31 +0800

    1

--- Chapter_1/1-2.cu
+++ Chapter_1/1-2.cu
@@ -3,10 +3,11 @@
 _global_ void helloFromGPU(void){
     printf("Hello World from GPU!\n");
 }
-int main(){
+
+int main(void){
     printf("Hello World from CPU!\n");
     helloFromGPU <<<1,10>>>();
     cudaDeviceReset();
     return 0;
-
+  
 }
