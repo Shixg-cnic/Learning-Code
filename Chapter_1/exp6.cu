d8427a6689cc7a2e48aeced1d732f67ed1f4d82e
Shixingguo <shixg@ShixingguodeMacBook-Air.local>
2025-09-17 10:52:38 +0800

    1

---
+++ Chapter_1/exp6.cu
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+__global__ void helloFromGPU(void){
+    printf("Hello World from GPU!\n");
+}
+
+__global__ void helloFromGPU1(void){
+    int a = threadIDx.x;
+    printf("Hello World from GPU %d\n", a);
+    
+}
+
+int main(void){
+    printf("Hello World from CPU!\n");
+    helloFromGPU1 <<<1,10>>>();
+    cudaDeviceReset();
+    return 0;
+  
+}
