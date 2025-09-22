a3b1ee2559dad1c89e1e49f356ae068fcc45276e
Shixingguo <shixg@ShixingguodeMacBook-Air.local>
2025-09-22 10:07:26 +0800

    1

--- Chapter_2/2-6.cu
+++ Chapter_2/2-6.cu
@@ -34,7 +34,7 @@ __global__ void printThreadIndex(int *A, const int nx, const int ny){
     int ix = threadIdx.x + blockIdx.x * blockDim.x;
     int iy = threadIdx.y + blockIdx.y * blockDim.y;
     unsigned int idx = ix + iy * nx;
-    printf("thread_id (%d,%d) block_id (%d,%d) cootdinate(%d,%d) globa lindex: %2d ival: %2d ",
+    printf("thread_id (%d,%d) block_id (%d,%d) cootdinate(%d,%d) globa lindex: %2d ival: %2d\n ",
             threadIdx.x,threadIdx.y,blockIdx.x,blockIdx.y,ix,iy,idx,A[idx]); 
 }
 
